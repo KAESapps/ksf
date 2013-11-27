1f43bb74f1f963217ee396dd177373e45431bf04
Quentin <quentin.vuilliot@kaes.fr>
2013-11-27 17:18:41 +0100

    TextInput enhanced

--- components/form/TextInput.js
+++ components/form/TextInput.js
@@ -10,14 +10,31 @@ define([
 	return compose(
 		CompositeMono,
 		function (args) {
-			var domEvents = {
+			var self = this,
+				domEvents = {
 				change: 'value',
 			};
-			if (args && args.updateFrequently) {
-				domEvents.keyup = 'value';
+			if (args) {
+				if (args.updateFrequently) {
+					domEvents.keyup = 'value';
+				}
+				// "fieldName" property is only useful when used in a form
+				args.fieldName && this.set('fieldName', args.fieldName);
+				args.passwordDisplay && this.set('passwordDisplay', args.passwordDisplay);
+				args.placeholder && this.set('placeholder', args.placeholder);
 			}
 			this._component = new HtmlElement('input', {type: 'text'}, null, domEvents);
 			this._component.domAttrs.bind('value', this, 'value');
+
+			this.getR('passwordDisplay').onValue(function(pass) {
+				self._component.domAttrs.set('type', pass ? 'password' : 'text');
+			});
+			this.getR('placeholder').onValue(function(placeholder) {
+				placeholder && self._component.domAttrs.set('placeholder', placeholder);
+			});
+			this.getR('fieldName').onValue(function(fieldName) {
+				fieldName && self._component.domAttrs.set('name', fieldName);
+			});
 		}
 	);
 });

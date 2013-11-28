35ef2c89b4f3489f171f3f0150a95a216d657c06
Quentin <quentin.vuilliot@kaes.fr>
2013-11-28 18:22:37 +0100

    TextInput: new properties

--- components/form/TextInput.js
+++ components/form/TextInput.js
@@ -22,6 +22,7 @@ define([
 				args.fieldName && this.set('fieldName', args.fieldName);
 				args.passwordDisplay && this.set('passwordDisplay', args.passwordDisplay);
 				args.placeholder && this.set('placeholder', args.placeholder);
+				args.hint && this.set('hint', args.hint);
 			}
 			this._component = new HtmlElement('input', {type: 'text'}, null, domEvents);
 			this._component.domAttrs.bind('value', this, 'value');
@@ -29,11 +30,14 @@ define([
 			this.getR('passwordDisplay').onValue(function(pass) {
 				self._component.domAttrs.set('type', pass ? 'password' : 'text');
 			});
+			this.getR('fieldName').onValue(function(fieldName) {
+				fieldName && self._component.domAttrs.set('name', fieldName);
+			});
 			this.getR('placeholder').onValue(function(placeholder) {
 				placeholder && self._component.domAttrs.set('placeholder', placeholder);
 			});
-			this.getR('fieldName').onValue(function(fieldName) {
-				fieldName && self._component.domAttrs.set('name', fieldName);
+			this.getR('hint').onValue(function(hint) {
+				hint && self._component.domAttrs.set('title', hint);
 			});
 		}
 	);

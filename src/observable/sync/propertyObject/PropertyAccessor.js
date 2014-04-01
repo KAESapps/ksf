5db1d9f4baa862c28512a1ce7855ee90b5d9e2aa
Quentin <quentin.vuilliot@kaes.fr>
2014-04-01 11:27:44 +0200

    PropertyAccessor's "value" event only when value has changed + "changes" event implemented

--- src/observable/sync/propertyObject/PropertyAccessor.js
+++ src/observable/sync/propertyObject/PropertyAccessor.js
@@ -19,6 +19,15 @@ define([
 		Trait.prototype._extractValue = function(parentValue) {
 			return parentValue && parentValue[this._propName];
 		};
+		Trait.prototype._extractChanges = function(parentChanges) {
+			var propName = this._propName,
+				changes;
+			var found = parentChanges && parentChanges.some(function(change) {
+				changes = change.arg;
+				return change.key === propName;
+			});
+			return changes;
+		};
 
 		Trait.prototype.value = function(value) {
 			if (arguments.length === 0) {
@@ -47,8 +56,18 @@ define([
 		Trait.prototype._onValue = function(listener) {
 			if (this._destroyed) { throw "Destroyed"; }
 			var self = this;
-			return this.own(this._parent.on('value', function(parentValue) {
-				listener(self._extractValue(parentValue));
+			return this.own(this._parent.on('changes', function(parentChanges) {
+				var changes = self._extractChanges(parentChanges);
+				changes && listener(self.value());
+			}));
+		};
+
+		Trait.prototype._onChanges = function(listener) {
+			if (this._destroyed) { throw "Destroyed"; }
+			var self = this;
+			return this.own(this._parent.on('changes', function(parentChanges) {
+				var changes = self._extractChanges(parentChanges);
+				changes && listener(changes);
 			}));
 		};
 

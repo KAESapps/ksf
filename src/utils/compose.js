3876ea72592fbb23b1654581f518afe012037d52
Sylvain Vuilliot <sylvain.vuilliot@kaes.fr>
2014-04-01 17:25:26 +0200

    compose.create

--- src/utils/compose.js
+++ src/utils/compose.js
@@ -3,6 +3,8 @@ define([
 ], function(
 
 ){
+
+
 	var compose = function(base) {
 		var constructors = [];
 		var prototypes = [];
@@ -35,5 +37,36 @@ define([
 		}
 		return Ctr;
 	};
+
+	compose.create = function(base) {
+		var trait, instance, i, l, props;
+		var constructors = [];
+		// inherit
+		if (typeof base === 'function') {
+			instance = Object.create(base.prototype);
+			constructors.push(base);
+		} else {
+			instance = Object.create(base);
+		}
+		// mixin properties
+		for (i = 1, l = arguments.length; i < l ; i++) {
+			trait = arguments[i];
+			if (typeof trait === 'function') {
+				constructors.push(trait);
+				props = trait.prototype;
+			} else {
+				props = trait;
+			}
+			for (var key in props) {
+				instance[key] = props[key];
+			}
+		}
+		// init instance
+		for (i = 0, l = constructors.length ; i < l ; i++) {
+			constructors[i].call(instance);
+		}
+		return instance;
+	};
+
 	return compose;
 });
--- src/utils/tests/compose.js
+++ src/utils/tests/compose.js
@@ -86,4 +86,79 @@ define([
 			assert.equal(syv.describe(), "My name is syv and I'm a dev");
 		},
 	});
+
+registerSuite({
+	name: "compose.create",
+	"one arg of type object": function() {
+		var syv = compose.create({
+			greet: function(somebody) {
+				return "Hello " + somebody;
+			}
+		});
+
+		assert.equal(syv.greet("John"), "Hello John");
+	},
+	"one arg of type function": function() {
+		var Person = function () {
+		};
+
+		var syv = compose.create(Person, {nom : 'syv'});
+		assert.equal(syv.nom, 'syv');
+	},
+	"one arg of type constructor": function() {
+		var Person = function (name) {
+		};
+		Person.prototype = {
+			greet: function(somebody) {
+				return "Hello " + somebody;
+			}
+		};
+
+		var syv = compose.create(Person, {name: 'syv'});
+		assert.equal(syv.name, 'syv');
+		assert.equal(syv.greet("John"), "Hello John");
+	},
+	"constructor and object": function() {
+		var Person = function (name) {
+		};
+		Person.prototype = {
+			greet: function(somebody) {
+				return "Hello " + somebody;
+			}
+		};
+
+		var Employee = {
+			doWork: function() {
+				return  "I'm working";
+			},
+		};
+
+		var syv = compose.create(Person, Employee, {name: 'syv'});
+		assert.equal(syv.name, 'syv');
+		assert.equal(syv.greet("John"), "Hello John");
+		assert.equal(syv.doWork(), "I'm working");
+	},
+	"many constructors": function() {
+		var Person = function (name) {
+		};
+		Person.prototype = {
+			greet: function(somebody) {
+				return "Hello " + somebody;
+			}
+		};
+
+		var WithJob = function(){
+		};
+		WithJob.prototype = {
+			describe: function() {
+				return  "My name is " + this.name + " and I'm a " + this.job;
+			},
+		};
+
+		var syv = compose.create(Person, WithJob, {name: 'syv', job: 'dev'});
+
+		assert.equal(syv.describe(), "My name is syv and I'm a dev");
+	},
+});
+
 });

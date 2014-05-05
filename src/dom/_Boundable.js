9cc95736a2d95264ae78b3a563a73c1bd5498185
Quentin <quentin.vuilliot@kaes.fr>
2014-05-05 12:15:04 +0200

    _Boundable issue fix: handle falsy value

--- src/dom/_Boundable.js
+++ src/dom/_Boundable.js
@@ -1,8 +1,8 @@
 define([], function() {
 	return {
 		bounds: function(bounds) {
-			var width = bounds.width || null;
-			var height = bounds.height || null;
+			var width = bounds && bounds.width || null;
+			var height = bounds && bounds.height || null;
 			var nodeStyle = this.domNode.style;
 			nodeStyle.width = isNaN(width) ? width : width && (width + 'px');
 			nodeStyle.height = isNaN(height) ? height : height && (height + 'px');

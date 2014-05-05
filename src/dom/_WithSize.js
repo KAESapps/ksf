c6e736abf52ad3fcedb78aef37db80ca3e13ba55
Quentin <quentin.vuilliot@kaes.fr>
2014-05-05 12:14:57 +0200

    WithSize taking not "border-box" nodes into account

--- src/dom/_WithSize.js
+++ src/dom/_WithSize.js
@@ -1,10 +1,24 @@
 define([], function() {
 	return {
 		size: function() {
-			var compStyle = getComputedStyle(this.domNode);
+			var cs = getComputedStyle(this.domNode),
+				bb = cs.getPropertyValue('box-sizing') === 'border-box',
+				height = parseFloat(cs.getPropertyValue('height')),
+				width = parseFloat(cs.getPropertyValue('width'));
+
+			if (!bb) {
+				height += parseFloat(cs.getPropertyValue('padding-top') || 0);
+				height += parseFloat(cs.getPropertyValue('padding-bottom') || 0);
+				height += parseFloat(cs.getPropertyValue('border-top-width') || 0);
+				height += parseFloat(cs.getPropertyValue('border-bottom-width') || 0);
+				width += parseFloat(cs.getPropertyValue('padding-left') || 0);
+				width += parseFloat(cs.getPropertyValue('padding-right') || 0);
+				width += parseFloat(cs.getPropertyValue('border-left-width') || 0);
+				width += parseFloat(cs.getPropertyValue('border-right-width') || 0);
+			}
 			return {
-				height: parseFloat(compStyle.getPropertyValue('height')),
-				width: parseFloat(compStyle.getPropertyValue('width'))
+				height: height,
+				width: width
 			};
 		}
 	};

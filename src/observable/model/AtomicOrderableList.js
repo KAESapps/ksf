05082226f1bd457d577ac85cf948286845292aad
Sylvain Vuilliot <sylvain.vuilliot@kaes.fr>
2014-05-05 11:46:21 +0200

    AtomicOrderableList

---
+++ src/observable/model/AtomicOrderableList.js
@@ -0,0 +1,15 @@
+define([
+	"compose",
+	'../computers/AtomicOrderableList',
+	'../accessorMixins/Value',
+], function(
+	compose,
+	Computer,
+	AccessorMixin
+){
+	var AtomicOrderableList = compose(function(itemModel) {
+		this.computer = new Computer(itemModel.computer);
+		this.accessorMixin = new AccessorMixin(itemModel.accessorMixin).ctr;
+	});
+	return AtomicOrderableList;
+});

bfc1331b2d95ca57c9db2e9fc1490d5757064bb5
Quentin <quentin.vuilliot@kaes.fr>
2014-05-22 17:32:06 +0200

    Fix _WithSize

--- src/dom/_WithSize.js
+++ src/dom/_WithSize.js
@@ -3,19 +3,33 @@ define([], function() {
 		size: function() {
 			var cs = getComputedStyle(this.domNode),
 				bb = cs.getPropertyValue('box-sizing') === 'border-box',
-				height = parseFloat(cs.getPropertyValue('height')),
-				width = parseFloat(cs.getPropertyValue('width'));
+				height = cs.getPropertyValue('height'),
+				width = cs.getPropertyValue('width');
 
-			if (!bb) {
-				height += parseFloat(cs.getPropertyValue('padding-top') || 0);
-				height += parseFloat(cs.getPropertyValue('padding-bottom') || 0);
-				height += parseFloat(cs.getPropertyValue('border-top-width') || 0);
-				height += parseFloat(cs.getPropertyValue('border-bottom-width') || 0);
-				width += parseFloat(cs.getPropertyValue('padding-left') || 0);
-				width += parseFloat(cs.getPropertyValue('padding-right') || 0);
-				width += parseFloat(cs.getPropertyValue('border-left-width') || 0);
-				width += parseFloat(cs.getPropertyValue('border-right-width') || 0);
+			if (height === 'auto') {
+				height = this.domNode.offsetHeight;
+			} else {
+				height = parseFloat(height);
+				if (!bb) {
+					height += parseFloat(cs.getPropertyValue('padding-top') || 0);
+					height += parseFloat(cs.getPropertyValue('padding-bottom') || 0);
+					height += parseFloat(cs.getPropertyValue('border-top-width') || 0);
+					height += parseFloat(cs.getPropertyValue('border-bottom-width') || 0);	
+				}
 			}
+
+			if (width === 'auto') {
+				width = this.domNode.offsetWidth;
+			} else {
+				width = parseFloat(width);
+				if (!bb) {
+					width += parseFloat(cs.getPropertyValue('padding-left') || 0);
+					width += parseFloat(cs.getPropertyValue('padding-right') || 0);
+					width += parseFloat(cs.getPropertyValue('border-left-width') || 0);
+					width += parseFloat(cs.getPropertyValue('border-right-width') || 0);
+				}
+			}
+
 			return {
 				height: height,
 				width: width

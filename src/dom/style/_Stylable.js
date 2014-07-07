db40fc2085e881bd624827e773ebe874f13a4740
Quentin <quentin.vuilliot@kaes.fr>
2014-07-07 11:49:58 +0200

    _Stylable does not allow setting a _style property on prototype anymore

--- src/dom/style/_Stylable.js
+++ src/dom/style/_Stylable.js
@@ -3,13 +3,10 @@ define([
 ], function(
 
 ){
-	var _Stylable = function() {
-		this._style && this.style(this._style);
-	};
-	_Stylable.prototype = {
+	var _Stylable = {
 		style: function(style) {
-			this.hasOwnProperty('_style') && this._style.unapply(this.domNode);
-			style.apply(this.domNode);
+			this._style && this._style.unapply(this.domNode);
+			style && style.apply(this.domNode);
 			this._style = style;
 			return this;
 		}

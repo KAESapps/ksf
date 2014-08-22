73e4a47199fa65d2ad1a9ee1dc1f8d8874484fac
Quentin <quentin.vuilliot@kaes.fr>
2014-08-22 10:04:49 +0200

    _Composite.inDom as getter & setter

--- src/dom/composite/_Composite.js
+++ src/dom/composite/_Composite.js
@@ -12,7 +12,7 @@ define([
 		this.domNode = this._root.domNode;
 	}, {
 		inDom: function(inDom) {
-			this._root.inDom && this._root.inDom(inDom);
+			return this._root.inDom && this._root.inDom(inDom);
 		},
 		bounds: function(bounds) {
 			this._root.bounds && this._root.bounds(bounds);

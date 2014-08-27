4e8a19bcdb4662c6620ed8fc3808610113d1fa5a
Sylvain <sylvain.vuilliot@kaes.fr>
2014-08-27 18:10:41 +0200

    dom/composite/_Focusable

---
+++ src/dom/composite/_Focusable.js
@@ -0,0 +1,49 @@
+define([
+
+], function(
+
+) {
+	return {
+		_addFocusable: function(cmp) {
+			var focusableCmps = this._focusableCmps || (this._focusableCmps = []);
+			focusableCmps.push([
+				cmp,
+				cmp.onBlur(this._emitBlur.bind(this)),
+				cmp.onFocus(this._emitFocus.bind(this)),
+			]);
+			return cmp;
+		},
+		_removeFocusable: function(cmp) {
+			var focusableCmps = this._focusableCmps;
+			focusableCmps.some(function(cmpCancelerPair, i) {
+				if (cmpCancelerPair[0] === cmp) {
+					cmpCancelerPair[1]();
+					cmpCancelerPair[2]();
+					focusableCmps.splice(i, 0);
+					return true;
+				}
+			});
+		},
+		_emitBlur: function() {
+			if (!this.focused()) {
+				this._emit('blur');
+			}
+		},
+		_emitFocus: function() {
+			if (!this.focused()) {
+				this._emit('focus');
+			}
+		},
+		focused: function() {
+			return this._focusableCmps.some(function(cmpCancelerPair) {
+				return cmpCancelerPair[0].focused();
+			});
+		},
+		onBlur: function(cb) {
+			return this._on('blur', cb);
+		},
+		onFocus: function(cb) {
+			return this._on('focus', cb);
+		},
+	};
+});

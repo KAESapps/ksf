21503d86d7f4e1530b2d882679243b464ebf9887
Sylvain <sylvain.vuilliot@kaes.fr>
2014-10-06 14:03:21 +0200

    remove compose map from require-config

--- require-config.js
+++ require-config.js
@@ -7,11 +7,6 @@ require.config({
 		{ name: 'es6-shim', location: 'es6-shim', main: 'es6-shim' },
 		{ name: 'Absurd', location: 'absurd/client-side/build', main: 'absurd.min' }
 	],
-	map: {
-		'ksf': {
-			compose: 'ksf/utils/compose'
-		}
-	}
 });
 
 if (typeof document !== "undefined") {

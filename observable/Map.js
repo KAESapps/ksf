b221b5b1f343c667a3241d4c57f7069675b3fb53
Sylvain <sylvain.vuilliot@kaes.fr>
2014-11-04 11:33:17 +0100

    Revert "Observable/Map is less strict and allows to get and set value"
    This reverts commit 0268cdde84186c89d92f47159e91086432355d9b.

--- observable/Map.js
+++ observable/Map.js
@@ -5,45 +5,24 @@ define([
 	compose,
 	_Evented
 ) {
-	// s'occupe uniquement du mapping clé/valeur mais sans s'occuper de la notion d'ajout ou de suppression de clé. En fait, toutes les clés existent théoriquement et pointent vers la valeur undefined par défaut jusq'à ce qu'on les fasse pointer vers autre chose.
-	// Dans une implémentation stricte, il ne serait pas possible d'itérer sur les clés non null (ce qui permettrait d'utiliser un WeakMap comme structure de sockage)
+	// s'occupe uniquement du mapping clé/valeur mais sans s'occuper de la notion d'ajout ou de suppression de clé. En fait, toutes les clés existent théoriquement et pointent vers la valeur null par défaut jusq'à ce qu'on les fasse pointer vers autre chose.
+	// Il n'est pas possible d'itérer sur les clés non null
 	return compose(_Evented, function(initValue) {
-		this._storage = initValue || {};
+		this._storage = initValue || {}; // à l'avenir ça pourrait être un weakMap
 	}, {
 		// @param change objet de la forme {key1: value1, key2: value2, ...}
 		change: function(change) {
 			var storage = this._storage;
 			Object.keys(change).forEach(function(key) {
-				var newKeyValue = change[key];
-				if (newKeyValue === undefined) {
-					delete storage[key];
-				} else {
-					storage[key] = newKeyValue;
-				}
+				storage[key] = change[key];
 			});
 			this._emit('change', change);
 		},
 		onChange: function(cb) {
 			return this._on('change', cb);
 		},
-		valueAt: function(key) {
-			return this._storage[key];
-		},
-		// permet de récupérer la liste des valeurs non null en mode getter
-		// permet également de réinitialiser les valeurs en mode setter, en passant à null toutes les clés a qui on n'affecte pas une valeur
-		// ça serait surement à sortir d'une implémentation un peu stricte qui voudrait utiliser un WeakMap
-		value: function(value) {
-			if (arguments.length) {
-				var change = value; // TODO: faire un clone pour plus de sécurité
-				Object.keys(this._storage).forEach(function(key) {
-					if (!(key in value)) {
-						change[key] = undefined;
-					}
-				}, this);
-				this.change(change);
-				return this;
-			}
-			return this._storage;
+		valueOfKey: function(key) {
+			return this._storage[key] || null;
 		},
 	});
 

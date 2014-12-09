8f9454067e97b94874183fd22457bca926b908f9
Sylvain <sylvain.vuilliot@kaes.fr>
2014-12-09 21:31:42 +0100

    allow omitting some callbacks in bindBranch

--- observable/deep/bindBranch.js
+++ observable/deep/bindBranch.js
@@ -5,11 +5,11 @@ define([
 ) {
 	return function(tree, cbs) {
 		var storage = {}; // permet aux cbs de renvoyer une valeur pour la stocker
-		var add = function(key) {
+		var add = cbs.add && function(key) {
 			var ret = cbs.add(key);
 			storage[key] = ret;
 		};
-		var remove = function(key) {
+		var remove = cbs.remove && function(key) {
 			cbs.remove(key, storage[key]);
 			delete storage[key];
 		};
@@ -19,14 +19,14 @@ define([
 			// je ne sais pas si c'est très utile...
 			cbs.init(Object.keys(tree.keys()));
 		} else {
-			Object.keys(tree.keys()).forEach(add);
+			add && Object.keys(tree.keys()).forEach(add);
 		}
-		var keyAddedHandler = tree.onKeyAdded(add);
-		var keyRemovedHandler = tree.onKeyRemoved(remove);
+		var keyAddedHandler = add && tree.onKeyAdded(add);
+		var keyRemovedHandler = remove && tree.onKeyRemoved(remove);
 
 		return function() {
-			keyAddedHandler();
-			keyRemovedHandler();
+			keyAddedHandler && keyAddedHandler();
+			keyRemovedHandler && keyRemovedHandler();
 		};
 	};
 

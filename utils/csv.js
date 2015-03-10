7936e1fd7c6722510f06cc52ad3b31f554f76991
Sylvain Vuilliot <sylvain.vuilliot@kaes.fr>
2015-03-10 12:07:26 +0100

    explicit locale for csv output

--- utils/csv.js
+++ utils/csv.js
@@ -3,31 +3,32 @@ define([
 ], function(
 
 ){
-	function value2csv (val) {
+	function value2csv (val, locale) {
 		if (typeof val === 'string') {
 			return '"' + val + '"';
 		}
 		if (typeof val === 'number') {
-			return '"' + val.toLocaleString() + '"';
+			return '"' + val.toLocaleString(locale) + '"';
 		}
 		if (typeof val === 'boolean') {
 			return val ? 1 : 0;
 		}
 		if (val instanceof Date) {
-			return '"' + val.toLocaleString() + '"';
+			return '"' + val.toLocaleString(locale) + '"';
 		}
 		return '';
 	}
 
-	function array2csvLine (array) {
-		return array.map(value2csv).toString() + '\r\n';
+	function array2csvLine (array, locale) {
+		return array.map(function(val) { return value2csv(val, locale); }).toString() + '\r\n';
 	}
 
 	/**
 	Convert an array of items (objects with same shape) to a CSV string
 	Column titles of header line are picked from first item if no columnsConfig is provided
 	*/
-	function convert2csv(array, columnsConfig) {
+	function convert2csv(array, columnsConfig, locale) {
+		locale = locale || 'fr';
 		if (!columnsConfig) {
 			columnsConfig = Object.keys(array[0]);
 		}
@@ -52,7 +53,7 @@ define([
 			var values = columns.map(function(col) {
 				return item[col.id];
 			});
-			return csv += array2csvLine(values);
+			return csv += array2csvLine(values, locale);
 		}, '');
 		return header + body;
 	}

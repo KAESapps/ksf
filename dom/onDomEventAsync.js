0e465ddc8536fe368a06ea5aed5730994ff9ca21
Quentin <quentin.vuilliot@kaes.fr>
2015-03-20 17:08:08 +0100

    onDomEventAsync/onDomEventAsyncMethod split

---
+++ dom/onDomEventAsync.js
@@ -0,0 +1,14 @@
+define([], function(){
+	return function(domNode, eventName, cb, capture) {
+		var self = this,
+			asyncCb = function(ev) {
+			setTimeout(function() {
+				cb.call(self, ev);
+			});
+		};
+		domNode.addEventListener(eventName, asyncCb, capture);
+		return function() {
+			domNode.removeEventListener(eventName, asyncCb, capture);
+		};
+	};
+});
--- dom/onDomEventAsyncMethod.js
+++ dom/onDomEventAsyncMethod.js
@@ -1,17 +1,7 @@
-define([], function() {
+define(['./onDomEventAsync'], function(onDomEvent) {
 	return function(eventName) {
 		return function(listener) {
-			var self = this;
-			var domNode = this.domNode,
-				cb = function(ev) {
-					setTimeout(function() {
-						listener.call(self, ev);
-					});
-				};
-			domNode.addEventListener(eventName, cb);
-			return function() {
-				domNode.removeEventListener(eventName, cb);
-			};
+			return onDomEvent(this.domNode, eventName, listener);
 		};
 	};
 });

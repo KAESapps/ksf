e5de3b046dceb1270c64af65ae03965cc3d081a4
Sylvain Vuilliot <sylvain.vuilliot@kaes.fr>
2015-07-28 15:00:36 +0200

    observable/Value with new evented API

--- observable/Value.js
+++ observable/Value.js
@@ -1,3 +1,23 @@
-import StatefulFactory from './StatefulFactory';
-import ValueModel from './model/Value';
-export default new StatefulFactory(new ValueModel()).ctr;
+import _Evented from '../base/_Evented';
+import compose from '../utils/compose';
+export default compose(_Evented, function(initValue){
+	this._value = initValue;
+}, {
+	value: function(newValue) {
+		if (arguments.length) {
+			return this.change(newValue);
+		} else {
+			return this._value;
+		}
+	},
+	change: function(newValue) {
+		this._value = newValue;
+		this._emit('change', newValue);
+	},
+	onChange: function(cb) {
+		return this._on('change', cb);
+	},
+	offChange: function(cb) {
+		return this._off('change', cb);
+	}
+})

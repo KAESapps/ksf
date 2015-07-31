bd113479e099e3efa0e02e3d69c06c9b0fc371ab
Sylvain Vuilliot <sylvain.vuilliot@kaes.fr>
2015-07-31 11:58:00 +0200

    optimize bindValue in case of non scope provided

--- observable/bindValue.js
+++ observable/bindValue.js
@@ -1,5 +1,9 @@
 export default function(value, cb, scope) {
-    scope = scope || null;
+  if (scope) {
     cb.call(scope, value.value());
     return value.onChange(cb.bind(scope));
+  } else {
+    cb(value.value());
+    return value.onChange(cb);
+  }
 };
